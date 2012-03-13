6bb77b3d111eb11e328121f1bd74eb33b476a2fb
Tero Parviainen <tero.parviainen@eficode.fi>
2012-03-13 11:31:38 +0200

    Reservation via Ajax, unobtrusive style

---
+++ app/assets/javascripts/reservations.js
@@ -0,0 +1,9 @@
+$(function() {
+
+  $('.reserve-link').on('ajax:success', function(evt, data, status, xhr) {
+    $('#reservation-form').hide();
+    $('#reservation-status').html('<span class="label important">Reserved by '+data.user.email+'</span>');
+    $('#reservation-links').html('<a href="/books/'+data.book_id+'/reservations/'+data.id+'/free" class="btn" data-method="put">Free</a>');
+  });
+  
+});
