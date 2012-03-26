42304c01dd19174144759039ea629371c02ff6e8
Tair Assimov <tair@assimov.net>
2012-03-26 10:20:06 +0300

    Setting up Gmail and application wide settings: * Gmail SMTP settings * Development mail interceptor * Refactor configuration settings * Load application wide settings on boot

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
 
-  default from: "from@example.com"
+  default from: CONFIG[:from_email]
 
   def welcome_email(user)
     @user = user
---
+++ config/initializers/0_load_config.rb
@@ -0,0 +1,1 @@
+CONFIG = YAML.load_file("#{Rails.root.to_s}/config/settings.yml")[Rails.env].symbolize_keys
