d88e3a1ffcbd149f57b12870bde06fe052f37401
Tero Parviainen <tero.parviainen@eficode.fi>
2012-03-29 11:16:50 +0300

    reset session during login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
+      reset_session
       session[:user_id] = user.id
       redirect_to root_path
     else
